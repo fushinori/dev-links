4f2b6cf06c5a60260c805b93d86e5bc9bc1665f7
Rishi <git@fushinori.me>
2025-07-10 22:20:51 +0530

    Change background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${instrumentSans.className} antialiased`}>
+      <body className={`${instrumentSans.className} antialiased bg-grey-100`}>
         {children}
       </body>
     </html>
