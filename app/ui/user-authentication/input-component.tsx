45abd8a928ee60e026c84a8152fb3f87bd71844c
Rishi <git@fushinori.me>
2025-07-10 22:36:16 +0530

    Add input component

---
+++ app/ui/user-authentication/input-component.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/app/lib/utils";
+import Image from "next/image";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon: string;
+  errors?: string[];
+}
+
+export default function Input({
+  icon,
+  errors,
+  className,
+  ...rest
+}: InputProps) {
+  return (
+    <div className="relative">
+      <input
+        className={cn(
+          "outline-none w-full border-grey-300 border-[1px] rounded-lg p-2 placeholder:text-grey-500 pl-10 focus:border-purple-400 focus:shadow-lg/65 focus:shadow-grey-300",
+          className,
+        )}
+        {...rest}
+      />
+      <Image
+        className="absolute top-[14px] left-[14px]"
+        src={icon}
+        width={16}
+        height={16}
+        alt=""
+        aria-hidden={true}
+      ></Image>
+      <div>{errors && errors[0]}</div>
+    </div>
+  );
+}
