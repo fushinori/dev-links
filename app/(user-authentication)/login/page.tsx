2962940c88fa32bd2629735bf314778b0db2d56d
Rishi <git@fushinori.me>
2025-07-21 14:57:13 +0530

    Refactor user authentication forms
    Move them into their respective page.tsx files

--- app/(user-authentication)/login/page.tsx
+++ app/(user-authentication)/login/page.tsx
@@ -1,5 +1,94 @@
-import LoginForm from "@/app/ui/user-authentication/login-form";
+"use client";
 
-export default function Login() {
-  return <LoginForm />;
+import Link from "next/link";
+import { Button } from "@/app/ui/button";
+import Input from "@/app/ui/user-authentication/input-component";
+import { login } from "@/app/lib/actions";
+import { LoginSchema } from "@/app/lib/types";
+
+import { parseWithZod } from "@conform-to/zod";
+import { useActionState } from "react";
+import { useForm } from "@conform-to/react";
+
+export default function LoginForm() {
+  const [lastResult, action] = useActionState(login, undefined);
+
+  const [form, fields] = useForm({
+    // Sync the result of last submission
+    lastResult,
+
+    // Reuse the validation logic on the client
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: LoginSchema });
+    },
+
+    // Validate for the first time when the user leaves focus
+    shouldValidate: "onBlur",
+
+    // Revalidate every time the user types
+    shouldRevalidate: "onInput",
+  });
+
+  return (
+    <form
+      id={form.id}
+      onSubmit={form.onSubmit}
+      action={action}
+      className="bg-white p-10 rounded-xl flex flex-col gap-10"
+      noValidate
+    >
+      <div>
+        <h1 className="text-grey-700 font-bold text-[2rem] mb-2">Login</h1>
+        <p className="text-grey-500">
+          Add your details to get back into the app.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="text-grey-700 text-[0.75rem]">
+            Email address
+          </label>
+
+          <Input
+            key={fields.email.key}
+            name={fields.email.name}
+            defaultValue={fields.email.defaultValue}
+            type="email"
+            placeholder="e.g alex@email.com"
+            icon="/icon-email.svg"
+            errors={fields.email.errors}
+            className={fields.email.errors && "border-red-400"}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="text-grey-700 text-[0.75rem]">
+            Password
+          </label>
+
+          <Input
+            key={fields.password.key}
+            name={fields.password.name}
+            defaultValue={fields.password.defaultValue}
+            type="password"
+            placeholder="Enter your password"
+            icon="/icon-password.svg"
+            errors={fields.password.errors}
+            className={fields.password.errors && "border-red-400"}
+          />
+        </div>
+      </div>
+      <Button className="bg-purple-400 hover:bg-purple-300 focus:bg-purple-300 focus:shadow-lg/50 focus:shadow-purple-300 hover:shadow-lg/50 hover:shadow-purple-300  outline-none">
+        Login
+      </Button>
+
+      <p className="text-grey-500 text-center">
+        Don&apos;t have an account?{" "}
+        <Link className="text-purple-400" href="/signup">
+          Create account
+        </Link>
+      </p>
+    </form>
+  );
 }
--- app/(user-authentication)/signup/page.tsx
+++ app/(user-authentication)/signup/page.tsx
@@ -1,5 +1,120 @@
-import SignUpForm from "@/app/ui/user-authentication/signup-form";
+"use client";
 
-export default function Login() {
-  return <SignUpForm />;
+import Link from "next/link";
+import { Button } from "@/app/ui/button";
+import Input from "@/app/ui/user-authentication/input-component";
+import { signUp } from "@/app/lib/actions";
+import { useActionState } from "react";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { SignUpSchema } from "@/app/lib/types";
+
+export default function SignUpForm() {
+  const [lastResult, action] = useActionState(signUp, undefined);
+
+  const [form, fields] = useForm({
+    // Sync the result of last submission
+    lastResult,
+
+    // Reuse the validation logic on the client
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: SignUpSchema });
+    },
+
+    // Validate for the first time when the user leaves focus
+    shouldValidate: "onBlur",
+
+    // Revalidate every time the user types
+    shouldRevalidate: "onInput",
+  });
+
+  return (
+    <form
+      id={form.id}
+      onSubmit={form.onSubmit}
+      action={action}
+      className="bg-white p-10 rounded-xl flex flex-col gap-10"
+      noValidate
+    >
+      <div>
+        <h1 className="text-grey-700 font-bold text-[2rem] mb-2">
+          Create account
+        </h1>
+        <p className="text-grey-500">
+          Let’s get you started sharing your links!
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="text-grey-700 text-[0.75rem]">
+            Email address
+          </label>
+
+          <Input
+            key={fields.email.key}
+            name={fields.email.name}
+            defaultValue={fields.email.defaultValue}
+            type="email"
+            placeholder="e.g alex@email.com"
+            icon="/icon-email.svg"
+            errors={fields.email.errors}
+            className={fields.email.errors && "border-red-400"}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="text-grey-700 text-[0.75rem]">
+            Create password
+          </label>
+
+          <Input
+            key={fields.password.key}
+            name={fields.password.name}
+            defaultValue={fields.password.defaultValue}
+            type="password"
+            placeholder="At least 8 characters"
+            icon="/icon-password.svg"
+            errors={fields.password.errors}
+            className={fields.password.errors && "border-red-400"}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="confirm-password"
+            className="text-grey-700 text-[0.75rem]"
+          >
+            Confirm password
+          </label>
+
+          <Input
+            key={fields.confirmPassword.key}
+            name={fields.confirmPassword.name}
+            defaultValue={fields.confirmPassword.defaultValue}
+            type="password"
+            placeholder="At least 8 characters"
+            icon="/icon-password.svg"
+            errors={fields.confirmPassword.errors}
+            className={fields.confirmPassword.errors && "border-red-400"}
+          />
+        </div>
+
+        <p className="text-grey-500 text-[0.75rem]">
+          Password must contain at least 8 characters
+        </p>
+      </div>
+
+      <Button className="bg-purple-400 hover:bg-purple-300 focus:bg-purple-300 focus:shadow-lg/50 focus:shadow-purple-300 hover:shadow-lg/50 hover:shadow-purple-300  outline-none">
+        Create new account
+      </Button>
+
+      <p className="text-grey-500 text-center">
+        Already have an account?{" "}
+        <Link className="text-purple-400" href="/login">
+          Login
+        </Link>
+      </p>
+    </form>
+  );
 }
