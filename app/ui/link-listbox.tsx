c8fbc30bcec8ee30cb35a2f066841c9947b0ed32
Rishi <git@fushinori.me>
2025-08-30 17:04:26 +0530

    Add basic drag-and-sort functionality

--- app/ui/link-listbox.tsx
+++ app/ui/link-listbox.tsx
@@ -13,7 +13,9 @@ import {
   ListboxOptions,
 } from "@headlessui/react";
 import { useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
 import { ValidWebsite, websites } from "@/app/lib/types";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   id: number;
@@ -27,93 +29,110 @@ export default function LinkListBox({ id, name, position, username }: Props) {
     websites.find((site) => site.name === name),
   );
 
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: position });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   console.log(id);
 
   return (
-    <Fieldset className="w-full bg-grey-100 rounded-xl p-5 flex flex-col gap-3 my-6">
-      <div className="flex justify-between">
-        <div className="flex gap-2">
-          <Image src="/icon-drag-and-drop.svg" alt="" height={16} width={16} />
-          <Legend className="font-bold text-grey-500">Link #{position}</Legend>
-        </div>
-        <button className="text-grey-500 cursor-pointer">Remove</button>
-      </div>
-
-      <Field>
-        <Label className="text-grey-700 text-xs block mb-1 truncate">
-          Platform
-        </Label>
-        <Listbox value={selectedWebsite} onChange={setSelectedWebsite}>
-          <ListboxButton className="group flex justify-between rounded-lg bg-white w-full min-w-[300px] py-3 px-4 text-grey-700 outline-none border border-grey-300 focus:border-purple-400 data-open:border-purple-400 focus:shadow-lg/65 focus:shadow-grey-300">
-            <div className="flex gap-2">
+    <div ref={setNodeRef} style={style}>
+      <Fieldset className="w-full bg-grey-100 rounded-xl p-5 flex flex-col gap-3 my-6">
+        <div className="flex justify-between">
+          <div className="flex gap-2">
+            <button className="cursor-move" {...attributes} {...listeners}>
               <Image
-                src={selectedWebsite!.icon}
+                src="/icon-drag-and-drop.svg"
                 alt=""
                 height={16}
                 width={16}
               />
-              <span>{selectedWebsite!.name}</span>
-            </div>
-            <Image
-              src="/icon-chevron-down.svg"
-              className="transition-transform duration-200 group-data-open:rotate-180"
-              alt=""
-              height={16}
-              width={16}
-            />
-          </ListboxButton>
-          <ListboxOptions
-            anchor={{ gap: 16, to: "bottom" }}
-            className="w-(--button-width) p-3 bg-white outline-none border border-grey-300 rounded-lg"
-          >
-            {websites.map((website) => (
-              <ListboxOption
-                key={website.id}
-                value={website}
-                className="text-gray-500 data-focus:text-purple-400 data-selected:text-purple-400 cursor-pointer"
-              >
-                <div className="flex gap-2 items-center">
-                  {/*Website icon 
-                We're using mask image to get the current text-color to change the icon color on focus or select*/}
-                  <div
-                    className="h-4 w-4 bg-current"
-                    style={{
-                      WebkitMaskImage: `url(${website.icon})`,
-                      WebkitMaskRepeat: "no-repeat",
-                      WebkitMaskPosition: "center",
-                      WebkitMaskSize: "contain",
-                      maskImage: `url(${website.icon})`,
-                      maskRepeat: "no-repeat",
-                      maskPosition: "center",
-                      maskSize: "contain",
-                    }}
-                  />
-                  <span>{website.name}</span>
-                </div>
-
-                {/* Divider */}
-                <div className="border-b-[1px] border-grey-300 my-3"></div>
-              </ListboxOption>
-            ))}
-          </ListboxOptions>
-        </Listbox>
-      </Field>
-
-      <Field>
-        <Label className="text-grey-700 text-xs block mb-1">Link</Label>
-        <div className="flex items-center rounded-lg border border-grey-300 bg-white w-full focus-within:border-purple-400 focus-within:shadow-lg/65 focus-within:shadow-grey-300">
-          {/* URL Prefix */}
-          <span className="pl-4 py-3 text-grey-700 truncate max-w-[50%]">
-            {selectedWebsite!.prefix}
-          </span>
-
-          <Input
-            type="url"
-            className="flex-1 w-0 min-w-0 border-none bg-transparent px-0 py-3 text-grey-700 outline-none"
-            defaultValue={username}
-          />
+            </button>
+            <Legend className="font-bold text-grey-500">Link #{id}</Legend>
+          </div>
+          <button className="text-grey-500 cursor-pointer">Remove</button>
         </div>
-      </Field>
-    </Fieldset>
+
+        <Field>
+          <Label className="text-grey-700 text-xs block mb-1 truncate">
+            Platform
+          </Label>
+          <Listbox value={selectedWebsite} onChange={setSelectedWebsite}>
+            <ListboxButton className="group flex justify-between rounded-lg bg-white w-full min-w-[300px] py-3 px-4 text-grey-700 outline-none border border-grey-300 focus:border-purple-400 data-open:border-purple-400 focus:shadow-lg/65 focus:shadow-grey-300">
+              <div className="flex gap-2">
+                <Image
+                  src={selectedWebsite!.icon}
+                  alt=""
+                  height={16}
+                  width={16}
+                />
+                <span>{selectedWebsite!.name}</span>
+              </div>
+              <Image
+                src="/icon-chevron-down.svg"
+                className="transition-transform duration-200 group-data-open:rotate-180"
+                alt=""
+                height={16}
+                width={16}
+              />
+            </ListboxButton>
+            <ListboxOptions
+              anchor={{ gap: 16, to: "bottom" }}
+              className="w-(--button-width) p-3 bg-white outline-none border border-grey-300 rounded-lg"
+            >
+              {websites.map((website) => (
+                <ListboxOption
+                  key={website.id}
+                  value={website}
+                  className="text-gray-500 data-focus:text-purple-400 data-selected:text-purple-400 cursor-pointer"
+                >
+                  <div className="flex gap-2 items-center">
+                    {/*Website icon 
+                We're using mask image to get the current text-color to change the icon color on focus or select*/}
+                    <div
+                      className="h-4 w-4 bg-current"
+                      style={{
+                        WebkitMaskImage: `url(${website.icon})`,
+                        WebkitMaskRepeat: "no-repeat",
+                        WebkitMaskPosition: "center",
+                        WebkitMaskSize: "contain",
+                        maskImage: `url(${website.icon})`,
+                        maskRepeat: "no-repeat",
+                        maskPosition: "center",
+                        maskSize: "contain",
+                      }}
+                    />
+                    <span>{website.name}</span>
+                  </div>
+
+                  {/* Divider */}
+                  <div className="border-b-[1px] border-grey-300 my-3"></div>
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Listbox>
+        </Field>
+
+        <Field>
+          <Label className="text-grey-700 text-xs block mb-1">Link</Label>
+          <div className="flex items-center rounded-lg border border-grey-300 bg-white w-full focus-within:border-purple-400 focus-within:shadow-lg/65 focus-within:shadow-grey-300">
+            {/* URL Prefix */}
+            <span className="pl-4 py-3 text-grey-700 truncate max-w-[50%]">
+              {selectedWebsite!.prefix}
+            </span>
+
+            <Input
+              type="url"
+              className="flex-1 w-0 min-w-0 border-none bg-transparent px-0 py-3 text-grey-700 outline-none"
+              defaultValue={username}
+            />
+          </div>
+        </Field>
+      </Fieldset>
+    </div>
   );
 }
--- app/ui/links-form.tsx
+++ app/ui/links-form.tsx
@@ -4,21 +4,64 @@ import { SecondaryButton } from "@/app/ui/button/button-secondary";
 import { dummyLinks } from "@/app/lib/dummy_data";
 import LinkListBox from "@/app/ui/link-listbox";
 import { useState } from "react";
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 export default function LinksForm() {
   const [links, setLinks] = useState(dummyLinks);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  const handleDragEnd = ({ active, over }: DragEndEvent) => {
+    if (over && active.id !== over.id) {
+      const oldIndex = links.findIndex((item) => item.id === active.id);
+      const newIndex = links.findIndex((item) => item.id === over.id);
+      setLinks(arrayMove(links, oldIndex, newIndex));
+    }
+  };
   return (
     <form>
       <SecondaryButton className="w-full mb-6">+ Add new link</SecondaryButton>
-      {links.map((link) => (
-        <LinkListBox
-          key={link.id}
-          id={link.id}
-          position={link.position}
-          name={link.name}
-          username={link.username}
-        />
-      ))}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={links.map((item) => item.id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {links.map((link) => (
+            <LinkListBox
+              key={link.id}
+              id={link.id}
+              position={link.position}
+              name={link.name}
+              username={link.username}
+            />
+          ))}
+        </SortableContext>
+      </DndContext>
     </form>
   );
 }
