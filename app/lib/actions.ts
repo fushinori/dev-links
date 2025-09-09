ebf4ab5495a87f9c40d27f7de5efd54b06cb4d9d
Rishi <git@fushinori.me>
2025-09-09 15:08:30 +0530

    Remove optional from betterAuthError

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -80,7 +80,7 @@ export async function login(
 ): Promise<
   | SubmissionResult<string[]>
   | (SubmissionResult & {
-      betterAuthError?: BetterAuthErrorMessage;
+      betterAuthError: BetterAuthErrorMessage;
       isAuthError: boolean;
     })
 > {
