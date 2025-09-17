1373b07943ce61fe79782897b0ee5a2169004033
Rishi <git@fushinori.dev>
2025-09-17 20:00:02 +0530

    Add utility functions
    - Function to generate a link from a UserLink by combining the prefixes and usernames
    - Function to return the correct styles for each website since we can't dynamically generate classes like `bg-`
    - Function to return the correct icons for each website

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { UserLink } from "@/app/lib/types";
+import { UserLink, ValidWebsite, websites } from "@/app/lib/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -15,4 +15,39 @@ export function updatePositions(links: UserLink[]): UserLink[] {
 
 export async function sendMockEmail(email: string, url: string) {
   console.log(`Email: ${email}\nURL: ${url}`);
+}
+
+const prefixMap: Record<ValidWebsite, string> = Object.fromEntries(
+  websites.map((w) => [w.name, w.prefix]),
+) as Record<ValidWebsite, string>;
+
+export function generateLink(userLink: UserLink): string {
+  return `${prefixMap[userLink.website]}${userLink.username}`;
+}
+
+const websiteBgMap: Record<string, string> = {
+  github: "bg-github",
+  frontendmentor: "bg-frontendmentor text-black border-[1px] border-grey-300",
+  twitter: "bg-twitter",
+  linkedin: "bg-linkedin",
+  youtube: "bg-youtube",
+  facebook: "bg-facebook",
+  twitch: "bg-twitch",
+  devto: "bg-devto",
+  codewars: "bg-codewars",
+  freecodecamp: "bg-freecodecamp",
+  codepen: "bg-codepen",
+  gitlab: "bg-gitlab",
+  hashnode: "bg-hashnode",
+  stackoverflow: "bg-stackoverflow",
+};
+
+export function getWebsiteClassName(name: string): string {
+  const sanitizedName = name.replace(/[\s.]/g, "").toLowerCase();
+  return websiteBgMap[sanitizedName] || "bg-black-400";
+}
+
+export function getWebsiteIcon(name: string): string {
+  const sanitizedName = name.replace(/[\s.]/g, "").toLowerCase();
+  return `/preview/${sanitizedName}.svg`;
 }
